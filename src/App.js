ca4942f92228e62199baf95ddc175e7ff1c951ee
Josh <leong.jo@gmail.com>
2018-01-05 14:53:25 -0700

    fix home

--- src/App.js
+++ src/App.js
@@ -6,12 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Beach Explorer</h1>
-        </header>
+          <h1 className="App-title">Whitsunday Beach</h1>
+        <img src ="https://www.cruisewhitsundays.com/media/28134/whitehaven-beach-day-cruise9.jpg?maxwidth=1280" alt="beach" />
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        Whitsunday Island, Australia
         </p>
       </div>
     );
